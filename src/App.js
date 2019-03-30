a6b625d54bddf8ffba4dbf9672e8dd4fcfea1eb2
soheb panjwani <sieger@sohebs-MBP.dal.corespace.com>
2019-03-30 14:27:12 -0500

    Added Album

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>
+            <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'> Library</Link>
+               <Link to='/album'> Album  </Link>
+           </nav>
+
+           <h1>Bloc Jams</h1>
+         </header>
+            <main>
+           <Route exact path="/" component={Landing} />
+           <Route path="/library" component={Library} />
+           <Route path="/album" component={Album} />
+         </main>
+
+
       </div>
     );
   }
